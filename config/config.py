492d2675f64fc2601dfc3fa366521ea02fbe2a8e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-11-21 22:54:31 +0330

    Fix Update

--- config/config.py
+++ config/config.py
@@ -1,16 +1,5 @@
-PRODUCTION = False
+import os
 
-DB = {
-    'influx_prod': {
-        'url': 'http://127.0.0.1:9098',
-        'token': 'xLh2BnswKUraKXJqWnOeHgrMIUdb61H2DqVIInVbWzivuB0nfBmIGb2leS3yZsLZOMFqAIKG5WFa8qDDfCtMgA==',
-        'org': 'BlackIQ',
-        'bucket': 'amir-monitoring',
-    },
-    'influx_local': {
-        'url': 'http://127.0.0.1:8086',
-        'token': 'aL7fOUnjYOMN1-aoyj3DMsMxJ44edgnN2Ts2Fv-uS5ZI2_CpsXo-17G57B5YMZLJ0wVuPoKIJ9d_HMHZOF9qPw==',
-        'org': 'BlackIQ',
-        'bucket': 'amir-monitoring',
-    }
-}
+API_ENDPOINT = os.getenv("API_ENDPOINT")
+SERVER_ID = os.getenv("SERVER_ID")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")

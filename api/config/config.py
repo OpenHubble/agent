211fe79a04abb3b13c76b9a6f9a73b5a50032508
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-15 16:29:42 +0330

    Now using a ini file to config the Agent variables

--- api/config/config.py
+++ api/config/config.py
@@ -1,2 +1,22 @@
-HOST_NAME = "MacBook-Pro-Amirhossein"
-APP_PORT = 19820
+# Import Config Parser
+import configparser
+
+# Specify Path and name
+CONFIG_FILE_NAME = "agent.ini"
+CONFIG_FILE_PATH = "/Users/amirhosseinmohammadi/Projects/amirhossein/amir-monitoring/agent"
+
+# Init Config
+config = configparser.ConfigParser()
+config.read(f"{CONFIG_FILE_PATH}/{CONFIG_FILE_NAME}")
+
+# ----- Read Configs ----- #
+
+# Server
+ALLOWED_IPS = config["Server"]["ALLOWED_IPS"].split(",")
+
+# Agent
+BIND_IP = config["Agent"]["BIND_IP"]
+PORT = config["Agent"]["PORT"]
+
+# Host
+HOST_NAME = config["Host"]["HOSTNAME"]

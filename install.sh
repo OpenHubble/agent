f8edede72cf5871cf951d75344a10ca393e042be
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-20 22:11:34 +0330

    Update file

--- install.sh
+++ install.sh
@@ -5,26 +5,26 @@ apt upgrade -y
 
 apt install python3 python3-venv python3-pip -y
 
-mkdir -p /apps
-cd /apps
+mkdir -p /opt
+cd /opt
 
 git clone https://gitlab.com/BlackIQ/amir-monitoring-agent agent
 
-cd /apps/agent
+cd /opt/agent
 
 cp -r agent.ini.example agent.ini
 
-# nano /apps/agent/agent.ini
+# nano /opt/agent/agent.ini
 
 mkdir -p /etc/agent
 
-cp -r /apps/agent/agent.ini /etc/agent
+cp -r /opt/agent/agent.ini /etc/agent
 
 python3 -m venv .venv
 
-/apps/agent/.venv/bin/python3 -m pip install -r requirements.txt
+/opt/agent/.venv/bin/python3 -m pip install -r requirements.txt
 
-cp /apps/agent/agent-monitoring.service /etc/systemd/system/
+cp /opt/agent/agent-monitoring.service /etc/systemd/system/
 
 systemctl daemon-reload
 

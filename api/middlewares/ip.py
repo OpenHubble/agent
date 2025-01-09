e53228265b67656a669110b618c819dae052a15a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-01-09 19:27:02 +0330

    Yes a simple update

--- api/middlewares/ip.py
+++ api/middlewares/ip.py
@@ -13,7 +13,10 @@ def allowed_ip(func):
         ALLOWED_IP_RANGES = [ipaddress.ip_network(ip.strip()) for ip in config.ALLOWED_IPS]
         
         if not any(ipaddress.ip_address(client_ip) in network for network in ALLOWED_IP_RANGES):
-            return jsonify({"error": "Forbidden", "message": "Your IP is not allowed"}), 403
+            return jsonify({
+                "error": "Forbidden",
+                "message": "Your IP is not allowed"
+            }), 403
         
         return func(*args, **kwargs)
 

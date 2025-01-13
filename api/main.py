1721f02f2d0d19b7cfd9588dfa4cb5a439ded13b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-01-13 19:12:20 +0330

    Fix imports + Add 2 new routes

--- api/main.py
+++ api/main.py
@@ -3,13 +3,13 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 # Import utils
-from api.utils.cpu import get_cpu_usage
-from api.utils.memory import get_memory_usage
-from api.utils.swap import get_swap_usage
-from api.utils.disk_io import get_disk_io
-from api.utils.network_io import get_network_io
-from api.utils.system_load import get_system_load
-from api.utils.disk_space import get_disk_space
+from api.utils.host.cpu import get_cpu_usage
+from api.utils.host.memory import get_memory_usage
+from api.utils.host.swap import get_swap_usage
+from api.utils.host.disk_io import get_disk_io
+from api.utils.host.network_io import get_network_io
+from api.utils.host.system_load import get_system_load
+from api.utils.host.disk_space import get_disk_space
 
 # Middlewares
 from api.middlewares.ip import allowed_ip
@@ -37,6 +37,50 @@ def ping():
 @allowed_ip
 @app.route('/api/metrics', methods=['GET'])
 def metrics():
+    response = {}
+    
+    metrics = {
+        "hostnme": config.HOST_NAME,
+        "version": config.AGENT_VERSION,
+        "cpu": get_cpu_usage(),
+        "memory": get_memory_usage(),
+        "swap": get_swap_usage(),
+        "disk_io": get_disk_io(),
+        "network_io": get_network_io(),
+        "system_load": get_system_load(),
+        "disk_space": get_disk_space(),
+    }
+
+    response['metrics'] = metrics
+
+    return jsonify(response), 200
+
+# Host Metrics
+@allowed_ip
+@app.route('/api/metrics/host', methods=['GET'])
+def host_metrics():
+    response = {}
+    
+    metrics = {
+        "hostnme": config.HOST_NAME,
+        "version": config.AGENT_VERSION,
+        "cpu": get_cpu_usage(),
+        "memory": get_memory_usage(),
+        "swap": get_swap_usage(),
+        "disk_io": get_disk_io(),
+        "network_io": get_network_io(),
+        "system_load": get_system_load(),
+        "disk_space": get_disk_space(),
+    }
+
+    response['metrics'] = metrics
+
+    return jsonify(response), 200
+
+# Docker Metrics
+@allowed_ip
+@app.route('/api/metrics/docker', methods=['GET'])
+def docker_metrics():
     response = {}
     
     metrics = {

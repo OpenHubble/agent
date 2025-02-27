9922b0a57e8187b18cacdd14477023c48260f111
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-02-27 12:48:54 +0330

    Migrate this middleware to fastapi

--- api/middlewares/ip.py
+++ api/middlewares/ip.py
@@ -1,23 +1,17 @@
-from flask import request, jsonify
-from functools import wraps
-
+from fastapi import HTTPException, Request, Depends
 import ipaddress
-
 import api.config.config as config
 
-def allowed_ip(func):
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
-        client_ip = request.remote_addr
-                
-        ALLOWED_IP_RANGES = [ipaddress.ip_network(ip.strip()) for ip in config.ALLOWED_IPS]
-        
-        if not any(ipaddress.ip_address(client_ip) in network for network in ALLOWED_IP_RANGES):
-            return jsonify({
-                "error": "Forbidden",
-                "message": "Your IP is not allowed"
-            }), 403
-        
-        return func(*args, **kwargs)
-
-    return decorated_function
+# Dependency function
+def allowed_ip(request: Request):
+    client_ip = request.client.host  # Get client IP
+    
+    # Convert ALLOWED_IPS to a list of ip_network objects
+    ALLOWED_IP_RANGES = [ipaddress.ip_network(ip.strip()) for ip in config.ALLOWED_IPS]
+    
+    # Check if client_ip is in any of the allowed IP ranges
+    if not any(ipaddress.ip_address(client_ip) in network for network in ALLOWED_IP_RANGES):
+        raise HTTPException(
+            status_code=403, 
+            detail="Forbidden: Your IP is not allowed"
+        )

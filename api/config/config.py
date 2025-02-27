479a45929fd15a4de6273390a90ee097494c12af
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-02-27 15:55:14 +0330

    Fixing reading toml file

--- api/config/config.py
+++ api/config/config.py
@@ -11,6 +11,9 @@ APP_MODE = os.getenv("AGENT_APP_MODE", "PRODUCTION")
 
 IS_PRODUCTION = True if str(APP_MODE) == "PRODUCTION" else False
 
+# Project Directory
+PROJECT_DIRECTORY = ""
+
 # Config file: Specify Path and name
 CONFIG_FILE_NAME = "openhubble-agent.ini"
 CONFIG_FILE_PATH = ""
@@ -24,11 +27,13 @@ PROJECT_CONFIG_FILE = "pyproject.toml"
 
 # Check path
 if IS_PRODUCTION:
+    PROJECT_DIRECTORY = "/opt/openhubble-agent"
     CONFIG_FILE_PATH = "/etc/openhubble-agent"
     LOG_FILE_PATH = "/var/log/openhubble-agent"
 else:
+    PROJECT_DIRECTORY = "./test"
     CONFIG_FILE_PATH = "./test"
-    LOG_FILE_PATH = "./test"
+    LOG_FILE_PATH = "."
     
 # Log file
 LOG_DESTINATION = f"{LOG_FILE_PATH}/{LOG_FILE_NAME}"
@@ -38,7 +43,7 @@ config = configparser.ConfigParser()
 config.read(f"{CONFIG_FILE_PATH}/{CONFIG_FILE_NAME}")
 
 # Read the TOML project config file
-project_config = toml.load(PROJECT_CONFIG_FILE)
+project_config = toml.load(f"{PROJECT_DIRECTORY}/{PROJECT_CONFIG_FILE}")
 
 # ----- Read Project Configs (TOML) ----- #
 PROJECT_NAME = project_config.get("tool", {}).get("openhubble", {}).get("name", "N/A")

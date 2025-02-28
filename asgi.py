1d54da9c93c075b4dbb3af976e579e224bd657ce
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-02-28 19:54:14 +0330

    Fix logging ERROR and ACCESS issues

--- asgi.py
+++ asgi.py
@@ -2,36 +2,43 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Use proccessing
-import multiprocessing
+# Cluster
+import multiprocessing # Multi processor
+import uvicorn # Uvicorn
 
-# Uvicorn
-import uvicorn
-
-# Main API
-from api.main import app
-
-# Config
-import api.config.config as config
+# APP
+from api.main import app # API
+import api.config.config as config # Config
 
 # Setup logging
-log_file = config.LOG_DESTINATION
+log_file = config.LOG_API_DESTINATION
 
+# File handler for logging
 handler = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=5)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
-handler.setLevel(logging.INFO)
 
+# Get the root logger and set its level to INFO
+root_logger = logging.getLogger()
+root_logger.addHandler(handler)
+root_logger.setLevel(logging.INFO)
+
+# Capture logs for uvicorn error and access logs
+uvicorn_error_logger = logging.getLogger("uvicorn.error")
+uvicorn_error_logger.addHandler(handler)
+uvicorn_error_logger.setLevel(logging.INFO)
+
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+uvicorn_access_logger.addHandler(handler)
+uvicorn_access_logger.setLevel(logging.INFO)
+
+# Log some initial information
 logger = logging.getLogger("uvicorn")
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
 logger.info("Agent application started!")
 logger.info(f"Agent Version: {config.AGENT_VERSION}")
 logger.info("OpenHubble By Amirhossein Mohammadi - 2025")
 
 # Run the ASGI server
 if __name__ == "__main__":
-    num_workers = multiprocessing.cpu_count() # Cound of proccessors
-    print(num_workers)
+    num_workers = multiprocessing.cpu_count()  # Number of processors
     uvicorn.run("asgi:app", host=config.BIND_IP, port=int(config.PORT), workers=num_workers)
